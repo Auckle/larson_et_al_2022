aa103840ea5788c908f5a23db4515c3d5dca0a6c
J. Larson <juliaclarson@gmail.com>
2022-02-18 19:54:47 -0800

    move configuration into main

--- crop_detections.py
+++ crop_detections.py
@@ -12,88 +12,17 @@ import sys
 import traceback
 
 
-##########################################################
-# Configuration
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "detections_json",
-    type=str,
-    default=None,
-    help="JSON file generated from the Megadetector run_tf_detector_batch.py script.",
-)
-
-parser.add_argument(
-    "img_metadata_csv", type=str, default=None, help="CSV file with img metadata"
-)
-
-parser.add_argument(
-    "input_dir", type=str, default=None, help="Input directory to raw images"
-)
-
-parser.add_argument(
-    "output_dir",
-    type=str,
-    default=None,
-    help="Output directory for cropped images and img metadata csv",
-)
-
-parser.add_argument(
-    "--detection_threshold",
-    type=float,
-    default=0.5,
-    help="Threshold for detections to use. Default is 0.5.",
-)
-parser.add_argument(
-    "--padding_factor",
-    type=float,
-    default=1.3 * 1.3,
-    help="We will crop a tight square box around the animal enlarged by this factor. "
-    + "Default is 1.3*1.3 = 1.69, which accounts for the cropping at test time and for"
-    + " a reason",
-)
-args = parser.parse_args()
-
-
-# Check arguments
-DETECTIONS_JSON = args.detections_json
-assert os.path.exists(DETECTIONS_JSON), DETECTIONS_JSON + " does not exist"
-
-IMAGE_METADATA_CSV = args.img_metadata_csv
-assert os.path.exists(IMAGE_METADATA_CSV), IMAGE_METADATA_CSV + " does not exist"
-
-INPUT_DIR = args.input_dir
-assert os.path.exists(INPUT_DIR), INPUT_DIR + " does not exist"
-
-OUTPUT_DIR = args.output_dir
-assert os.path.exists(OUTPUT_DIR), OUTPUT_DIR + " does not exist"
-
-OUTPUT_CSV = os.path.join(OUTPUT_DIR, "wellington_camera_traps_detections.csv")
-
-# Detection threshold should be in [0,1]
-DETECTION_THRESHOLD = args.detection_threshold
-assert (
-    DETECTION_THRESHOLD >= 0 and DETECTION_THRESHOLD <= 1
-), "Detection threshold should be in [0,1]"
-
-# Padding around the detected objects when cropping
-# 1.3 for the cropping during test time and 1.3 for
-# the context that the CNN requires in the left-over
-# image
-PADDING_FACTOR = args.padding_factor
-assert PADDING_FACTOR >= 1, "Padding factor should be equal or larger 1"
-
 
 ##########################################################
 # Functions
 
 
-def save_progress(output_db=pd.DataFrame):
+def save_progress(output_db=pd.DataFrame, output_csv=str):
     print("\n\n######## INFO: Saving cropping progress")
-    output_db.to_csv(OUTPUT_CSV)
+    output_db.to_csv(output_csv)
 
 
-def img_crop_detection(img_file=str, output_file=str, bbox=list):
+def img_crop_detection(output_dir=str, img_file=str, output_file=str, bbox=list, padding_factor=float):
 
     # Load image
     img = Image.open(img_file)
@@ -104,11 +33,11 @@ def img_crop_detection(img_file=str, output_file=str, bbox=list):
     # Get these boxes and convert normalized coordinates to pixel coordinates
     cords_box = bbox * np.tile(imsize, 2)
 
-    # Pad the detected animal to a square box and additionally by PADDING_FACTOR, the
+    # Pad the detected animal to a square box and additionally by padding factor, the
     # result will be in crop_boxes. However, we need to make sure that it box
     # coordinates are still within the image
     bbox_size = cords_box[-2:]
-    offsets = (PADDING_FACTOR * np.max([bbox_size], axis=1) - bbox_size) / 2
+    offsets = (padding_factor * np.max([bbox_size], axis=1) - bbox_size) / 2
     crop_box = cords_box + np.hstack([-offsets, offsets])
     crop_box = np.maximum(0, crop_box).astype(int)
 
@@ -117,7 +46,7 @@ def img_crop_detection(img_file=str, output_file=str, bbox=list):
         crop_box[1] : crop_box[1] + crop_box[3], crop_box[0] : crop_box[0] + crop_box[2]
     ]
 
-    output_img_file = os.path.join(OUTPUT_DIR, output_file)
+    output_img_file = os.path.join(output_dir, output_file)
     save_img = Image.fromarray(cropped_img)
     save_img = save_img.resize((mc.CROP_SIZE_W_PADDING, mc.CROP_SIZE_W_PADDING))
     save_img.save(output_img_file)
@@ -131,12 +60,84 @@ if __name__ == "__main__":
     print("\n##########################################################")
     print("##### START #####\n")
 
-    input_db = pd.read_csv(IMAGE_METADATA_CSV)
+    ##########################################################
+    # Configuration
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "detections_json",
+        type=str,
+        default=None,
+        help="JSON file generated from the Megadetector run_tf_detector_batch.py script.",
+    )
+
+    parser.add_argument(
+        "img_metadata_csv", type=str, default=None, help="CSV file with img metadata"
+    )
+
+    parser.add_argument(
+        "input_dir", type=str, default=None, help="Input directory to raw images"
+    )
+
+    parser.add_argument(
+        "output_dir",
+        type=str,
+        default=None,
+        help="Output directory for cropped images and img metadata csv",
+    )
+
+    parser.add_argument(
+        "--detection_threshold",
+        type=float,
+        default=0.5,
+        help="Threshold for detections to use. Default is 0.5.",
+    )
+    parser.add_argument(
+        "--padding_factor",
+        type=float,
+        default=1.3 * 1.3,
+        help="We will crop a tight square box around the animal enlarged by this factor. "
+        + "Default is 1.3*1.3 = 1.69, which accounts for the cropping at test time and for"
+        + " a reason",
+    )
+    args = parser.parse_args()
+
+
+    # Check arguments
+    detections_json = args.detections_json
+    assert os.path.exists(detections_json), detections_json + " does not exist"
+
+    image_metadata_csv = args.img_metadata_csv
+    assert os.path.exists(image_metadata_csv), image_metadata_csv + " does not exist"
+
+    input_dir = args.input_dir
+    assert os.path.exists(input_dir), input_dir + " does not exist"
+
+    output_dir = args.output_dir
+    assert os.path.exists(output_dir), output_dir + " does not exist"
+
+    output_csv = os.path.join(output_dir, "wellington_camera_traps_detections.csv")
+
+    # Detection threshold should be in [0,1]
+    detection_threshold = args.detection_threshold
+    assert (
+        detection_threshold >= 0 and detection_threshold <= 1
+    ), "Detection threshold should be in [0,1]"
+
+    # Padding around the detected objects when cropping
+    # 1.3 for the cropping during test time and 1.3 for
+    # the context that the CNN requires in the left-over
+    # image
+    padding_factor = args.padding_factor
+    assert padding_factor >= 1, "Padding factor should be equal or larger 1"
+
+
+    input_db = pd.read_csv(image_metadata_csv)
     input_db = input_db.set_index(["file"])
 
-    if os.path.exists(OUTPUT_CSV):
+    if os.path.exists(output_csv):
         print("\n##### Loading existing output csv file.")
-        output_db = pd.read_csv(OUTPUT_CSV)
+        output_db = pd.read_csv(output_csv)
         output_db = output_db.set_index(["file"])
     else:
         print("\n##### Creating new output database.")
@@ -147,27 +148,27 @@ if __name__ == "__main__":
         output_db = pd.DataFrame(columns=output_cols)
         output_db = output_db.set_index(["file"])
 
-    detections_json = json.load(open(DETECTIONS_JSON))
+    detections_json = json.load(open(detections_json))
     detections_db = pd.DataFrame(detections_json["images"])
     detections_db = detections_db.set_index(["file"])
 
-    image_list = glob.glob(INPUT_DIR + "/*.JPG")
+    image_list = glob.glob(input_dir + "/*.JPG")
 
     print(
-        "\nDETECTIONS_JSON:",
-        DETECTIONS_JSON,
-        "\nIMAGE_METADATA_CSV:",
-        IMAGE_METADATA_CSV,
-        "\nINPUT_DIR:",
-        INPUT_DIR,
-        "\nOUTPUT_DIR:",
-        OUTPUT_DIR,
-        "\nOUTPUT_CSV:",
-        OUTPUT_CSV,
-        "\nDETECTION_THRESHOLD:",
-        DETECTION_THRESHOLD,
-        "\nPADDING_FACTOR:",
-        PADDING_FACTOR,
+        "\detections json:",
+        detections_json,
+        "\image metadata csv:",
+        image_metadata_csv,
+        "\input dir:",
+        input_dir,
+        "\output dir:",
+        output_dir,
+        "\output csv:",
+        output_csv,
+        "\detection threshold:",
+        detection_threshold,
+        "\padding factor:",
+        padding_factor,
         "\n",
     )
     print("     input database:", input_db.columns.values)
@@ -218,7 +219,7 @@ if __name__ == "__main__":
                     raw_empty_imgs += 1
                     # print("    # CROPPING IMG W/ NO ANIMAL - ", org_file_name, input_row['label'])
                     # use the bounding boxes from the last non-empty image to generate a cropped empty image
-                    img_crop_detection(img_file, new_file_name, last_detection_cords)
+                    img_crop_detection(output_dir, img_file, new_file_name, last_detection_cords, padding_factor)
                     row = [
                         input_row["sequence"],
                         input_row["image_sequence"],
@@ -249,9 +250,9 @@ if __name__ == "__main__":
                                 + ".JPG"
                             )
 
-                            if not detection["conf"] > DETECTION_THRESHOLD:
+                            if not detection["conf"] > detection_threshold:
                                 detections_thresh_too_low += 1
-                                # print("   # SKIP - Detection threshold", detection['conf'], " is not greater than selected threshold ", DETECTION_THRESHOLD)
+                                # print("   # SKIP - Detection threshold", detection['conf'], " is not greater than selected threshold ", detection_threshold)
                                 continue
 
                             if new_file_name in output_db.index:
@@ -262,7 +263,7 @@ if __name__ == "__main__":
 
                             # print("    # CROPPING IMG W/ ANIMAL - ", new_file_name, input_row['label'])
                             img_crop_detection(
-                                img_file, new_file_name, detection["bbox"]
+                                output_dir, img_file, new_file_name, detection["bbox"], padding_factor
                             )
                             last_detection_cords = detection["bbox"]
                             row = [
@@ -286,15 +287,15 @@ if __name__ == "__main__":
                 # print("##### ERROR No metadata found for img", org_file_name)
 
     except KeyboardInterrupt:
-        save_progress(output_db)
+        save_progress(output_db, output_csv)
         raise
     except:
         print("##### ERROR in cropping process", sys.exc_info())
         print(traceback.format_exc())
-        save_progress(output_db)
+        save_progress(output_db, output_csv)
         raise
 
-    save_progress(output_db)
+    save_progress(output_db, output_csv)
 
     print(
         "\n##### Summary #####\n",
@@ -312,7 +313,7 @@ if __name__ == "__main__":
         "\n",
         detections_thresh_too_low,
         "detections did not meet the confidence threshold of ",
-        DETECTION_THRESHOLD,
+        detection_threshold,
         ".\n",
         detections_processed,
         "detections processed this run.",
@@ -328,7 +329,7 @@ if __name__ == "__main__":
         "\n",
     )
 
-    print("##### Output csv saved to", OUTPUT_CSV)
+    print("##### Output csv saved to", output_csv)
     print(output_db.head(10), "\n")
     print("##### output_db\n", output_db["label"].value_counts())
     print("\n###### input_db\n", input_db["label"].value_counts())
